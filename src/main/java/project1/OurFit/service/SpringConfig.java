3ffdbce809fb6c2f34fe6ab1fb7df7a191c6f6fe
parkjungmin <aossuper7@naver.com>
2023-04-14 14:47:29 +0900

    feat: Spring Config 설정

---
+++ src/main/java/project1/OurFit/service/SpringConfig.java
@@ -0,0 +1,27 @@
+package project1.OurFit.service;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import project1.OurFit.repository.JpaMemberRepository;
+import project1.OurFit.repository.MemberRepository;
+
+@Configuration
+public class SpringConfig {
+
+    private EntityManager em;
+
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JpaMemberRepository(em);
+    }
+}
