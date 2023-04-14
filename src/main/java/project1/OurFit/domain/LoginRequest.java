6af324ed443460be0c68c7b685e5511d0c5b6c07
parkjungmin <aossuper7@naver.com>
2023-04-14 14:43:45 +0900

    feat: LoginRequest DTO 생성

---
+++ src/main/java/project1/OurFit/domain/LoginRequest.java
@@ -0,0 +1,10 @@
+package project1.OurFit.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LoginRequest {
+    private String email;
+    private String password;
+}
