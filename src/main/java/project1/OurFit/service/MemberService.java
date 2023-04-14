9f4066901d89bfc834fcf9d4f28ac372a6e6061f
parkjungmin <aossuper7@naver.com>
2023-04-14 14:37:57 +0900

    feat: MemberService 생성

---
+++ src/main/java/project1/OurFit/service/MemberService.java
@@ -0,0 +1,21 @@
+package project1.OurFit.service;
+
+import org.springframework.transaction.annotation.Transactional;
+import project1.OurFit.domain.Member;
+import project1.OurFit.repository.MemberRepository;
+
+import java.util.Optional;
+
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Optional<Member> findEmailAndPassword(String email, String password) {
+        return memberRepository.findByEmailAndPassword(email, password);
+    }
+}
