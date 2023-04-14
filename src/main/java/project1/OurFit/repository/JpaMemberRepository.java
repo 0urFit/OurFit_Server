5ea1345bccc7b5c0879c8225e8e344d6808cd882
parkjungmin <aossuper7@naver.com>
2023-04-14 14:36:27 +0900

    feat: MemberRepository 생성

---
+++ src/main/java/project1/OurFit/repository/JpaMemberRepository.java
@@ -0,0 +1,23 @@
+package project1.OurFit.repository;
+
+import jakarta.persistence.EntityManager;
+import project1.OurFit.domain.Member;
+
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository {
+
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Optional<Member> findByEmailAndPassword(String email, String password) {
+        return em.createQuery("select m from Member m where m.Email = :email And m.Pwd = :password", Member.class)
+                .setParameter("email", email)
+                .setParameter("password", password)
+                .getResultStream().findAny();
+    }
+}
