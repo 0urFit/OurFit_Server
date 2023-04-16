01a99cd9f0a852a90781c0706e2ddad3885dc630
parkjungmin <aossuper7@naver.com>
2023-04-16 14:44:42 +0900

    feat: DTO 생성자 추가

--- src/main/java/project1/OurFit/domain/Member.java
+++ src/main/java/project1/OurFit/domain/Member.java
@@ -2,11 +2,18 @@ package project1.OurFit.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
 @Entity
+@NoArgsConstructor
 public class Member {
+    public Member(String email, String nickname, boolean gender) {
+        Email = email;
+        Nickname = nickname;
+        Gender = gender;
+    }
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
