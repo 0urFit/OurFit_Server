e441a9ff7b3c66e4ec65557cd69f6d8859bbbc59
parkjungmin <aossuper7@naver.com>
2023-04-16 14:45:32 +0900

    feat: Kakao login controller 추가

--- src/main/java/project1/OurFit/controller/SignInUp.java
+++ src/main/java/project1/OurFit/controller/SignInUp.java
@@ -2,9 +2,12 @@ package project1.OurFit.controller;
 
 import constant.JsonCode;
 import constant.JsonMessage;
+import constant.Oauth;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import project1.OurFit.domain.JsonResponse;
+import project1.OurFit.domain.KakaoProfile;
 import project1.OurFit.domain.LoginRequest;
 import project1.OurFit.domain.Member;
 import project1.OurFit.service.MemberService;
@@ -47,5 +50,25 @@ public class SignInUp {
         return memberService.join(member)
                 .map(m -> new JsonResponse(true, JsonCode.SUCCESS.getNum(), JsonMessage.SUCCESS.getMessage()))
                 .orElse(new JsonResponse(false, JsonCode.FAIL.getNum(), JsonMessage.FAIL.getMessage()));
+    }
+
+    @GetMapping("/oauth/kakao")
+    public String kakao() {
+        return "redirect:" + Oauth.KAKAOLOGIN.getValue();
+    }
+
+    @GetMapping("/auth/kakao/callback")
+    @ResponseBody
+    public JsonResponse oauthKakaoLogin(String code) {
+        KakaoProfile info = memberService.loginKakao(code);
+        return memberService.findEmail(info.getKakao_account().getEmail())
+                .map(m -> new JsonResponse(true, JsonCode.SUCCESS.getNum(), JsonMessage.SUCCESS.getMessage(),
+                        new LoginRequest(info.getKakao_account().getEmail())))
+                .orElseGet(() -> {
+                    boolean gender = info.getKakao_account().getGender().equals("male");
+                    return new JsonResponse(false, JsonCode.FAIL.getNum(), JsonMessage.FAIL.getMessage(),
+                            new Member(info.getKakao_account().getEmail(), info.getProperties().getNickname(),
+                                    gender));
+                });
     }
 }
