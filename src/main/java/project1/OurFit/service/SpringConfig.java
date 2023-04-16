a5d9fe777e1f81b9731f0605e1da8b6518226cd9
parkjungmin <aossuper7@naver.com>
2023-04-16 14:43:46 +0900

    feat: Spring 설정 kakaoRepository 추가

--- src/main/java/project1/OurFit/service/SpringConfig.java
+++ src/main/java/project1/OurFit/service/SpringConfig.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import project1.OurFit.repository.JpaMemberRepository;
+import project1.OurFit.repository.KakaoRepository;
 import project1.OurFit.repository.MemberRepository;
 
 @Configuration
@@ -17,11 +18,16 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository(), kakaoRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
         return new JpaMemberRepository(em);
+    }
+
+    @Bean
+    public KakaoRepository kakaoRepository() {
+        return new KakaoRepository();
     }
 }
