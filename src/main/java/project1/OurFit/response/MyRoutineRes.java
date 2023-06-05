f5d8a7555e586cc7877b57a11c5b493e7988a3f4
JinGyuGwak <104514223+JinGyuGwak@users.noreply.github.com>
2023-06-05 20:19:42 +0900

    refactor: 저장한 운동 루틴 list 응답값 추가 (API 5-1)

--- src/main/java/project1/OurFit/response/MyRoutineRes.java
+++ src/main/java/project1/OurFit/response/MyRoutineRes.java
@@ -12,11 +12,17 @@ public class MyRoutineRes {
     private int level;
     private int fewTime;
     private int period;
+    private String imgpath;
+    private String category;
+    private Long exerciseId;
 
     public MyRoutineRes(ExerciseRoutine ex){
         this.routineName=ex.getRoutineName();
         this.level=ex.getLevel();
         this.fewTime=ex.getFewTime();
         this.period=ex.getPeriod();
+        this.imgpath=ex.getImgpath();
+        this.category=ex.getCategory();
+        this.exerciseId=ex.getId();
     }
 }
