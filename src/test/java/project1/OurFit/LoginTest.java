9d17e4e7a61c4ac3e79b0c0e402db358ad1bc8b7
parkjungmin <aossuper7@naver.com>
2023-10-01 02:10:03 +0900

    test: REST DOCS login

--- src/test/java/project1/OurFit/LoginTest.java
+++ src/test/java/project1/OurFit/LoginTest.java
@@ -1,5 +1,6 @@
 package project1.OurFit;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,31 +8,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
+import project1.OurFit.request.LoginDTO;
 import project1.RestDocsConfiguration;
 
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Transactional
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @Import(RestDocsConfiguration.class)
-public class ExistsEmailAndNicknameTest {
+public class LoginTest {
     @Autowired private RestDocumentationResultHandler restDocs;
     private MockMvc mockMvc;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeEach
     void setUp(
@@ -40,35 +44,28 @@ public class ExistsEmailAndNicknameTest {
     ) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
-                .defaultRequest(get("/").header(HttpHeaders.HOST, "43.200.180.163:8080"))
+                .defaultRequest(post("/").header(HttpHeaders.HOST, "43.200.180.163:8080"))
                 .alwaysDo(restDocs)
                 .build();
     }
 
     @Test
-    void existEmail() throws Exception {
-        //When & Then
-        mockMvc.perform(get("/checkemail")
-                    .queryParam("email", "goodproject@naver.com"))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andDo(restDocs.document(
-                        queryParameters(
-                                parameterWithName("email").description("이메일")
-                        )
-                ));
-    }
+    void loginSuccess() throws Exception {
+        //Given
+        LoginDTO loginDTO = new LoginDTO();
+        loginDTO.setEmail("aossuper7@naver.com");
+        loginDTO.setPassword("aossuper7");
 
-    @Test
-    void existNickname() throws Exception {
         //When & Then
-        mockMvc.perform(get("/checknick")
-                    .queryParam("nick", "goodproject"))
+        mockMvc.perform(post("/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(loginDTO)))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andDo(restDocs.document(
-                        queryParameters(
-                                parameterWithName("nick").description("닉네임")
+                        requestFields(
+                                fieldWithPath("email").description("이메일"),
+                                fieldWithPath("password").description("비밀번호")
                         )
                 ));
     }
